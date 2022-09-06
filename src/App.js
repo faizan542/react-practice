72441a9e2f3124d3cf7076da991c55ad953c14e3
faizan542 <faizansilat100@gmail.com>
2022-09-06 16:03:24 +0500

    App Js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+ class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <h1>Header</h1>
+        </div>
+    )
+  }
 }
 
-export default App;
+class Footer extends Component{
+  render(){
+    return(
+      <div className='footer'>
+        <h1>Footer</h1>
+        </div>
+    )
+  }
+}
+
+export default class App extends Component {
+  render() {
+    let todos = [{ text: "Learn react" }, { text: "Go to Gym" }]
+    return (
+      <div>
+        <Header />
+        <ul className='todos'>
+          {todos.map((item, index) => {
+            return <li key={index}>{`${index + 1}  ${item.text}`}</li>
+          })}
+        </ul>
+        <Footer />
+      </div>
+    )
+  }
+}
