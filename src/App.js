a55c1762d48168f1e5296ac0bd03d9d42a99b859
faizan542 <faizansilat100@gmail.com>
2022-09-06 22:04:24 +0500

    App Js file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react'
-import './App.css'
+import './App.css';
+import ComOne from './components/ComOne';
 
- class Header extends Component {
+class Header extends Component {
   render() {
     return (
       <div className='header'>
         <h1>Header</h1>
-        </div>
+      </div>
     )
   }
 }
 
-class Footer extends Component{
-  render(){
-    return(
+class Footer extends Component {
+  render() {
+    return (
       <div className='footer'>
         <h1>Footer</h1>
-        </div>
+      </div>
     )
   }
 }
@@ -27,11 +28,14 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+
+        <h1 style={{ fontStyle: "italic", fontFamily: "fantasy", backgroundColor: "pink" }}>Main Component</h1>
         <ul className='todos'>
           {todos.map((item, index) => {
             return <li key={index}>{`${index + 1}  ${item.text}`}</li>
           })}
         </ul>
+        <ComOne />
         <Footer />
       </div>
     )
