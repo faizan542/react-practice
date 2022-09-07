b8de5cd190c8028e811bc70291e0040783f6cf57
faizan542 <faizansilat100@gmail.com>
2022-09-07 21:22:30 +0500

    added component

--- src/components/ComOne.jsx
+++ src/components/ComOne.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap';
 
 export default class ComOne extends Component {
     render() {
         return (
             <div>
                 <h1>Hello World</h1>
+                <button className='btn btn-primary'>Primary</button> <br /><br />
+                <Button as="a" variant="info">
+                    Button as link
+                </Button>
             </div>
         )
     }
