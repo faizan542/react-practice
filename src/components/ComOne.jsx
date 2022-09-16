058865dc3fcc726005ce868eeba985cd05a30b1a
faizan542 <faizansilat100@gmail.com>
2022-09-16 18:16:18 +0500

    ComOne file

--- src/components/ComOne.jsx
+++ src/components/ComOne.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Header from './Header';
 import { Button } from 'react-bootstrap';
 import ManakinBird from '../images/manakin.jpg';
 class ComOne extends Component {
@@ -17,7 +18,7 @@ class ComOne extends Component {
         })
     }
 
-    
+
     get_name = () => {
         console.log(this.state.myName)
     }
@@ -30,21 +31,31 @@ class ComOne extends Component {
         console.log(e.target.value)
     };
 
+
+    get_props = (props) => {
+        console.log(props)
+    }
+
     render() {
         return (
             <div>
+                <Header getProps={this.get_props} name={this.state.myName} page="Navbar page" />
+
                 <h1>Hello World</h1>
+
                 <Button as="a" variant="info">
                     Button as link
                 </Button>
                 <br /><br />
+
                 {/* <input type="text" placeholder='enter your name' onChange={(e)=>this.setState({myName: e.target.value})}/> <br /><br /> */}
                 {/* <input type="text" placeholder='enter your name' onChange={(e) => this.setState({ myValue: e.target.value })} /> <br /><br /> */}
-                <input type="text" name='name' placeholder='enter your name' onChange={(e) => this.set_value(e)} /> <br /><br />
+
+                <input type="text" name='myName' placeholder='enter your name' onChange={(e) => this.set_value(e)} /> <br /><br />
                 <input type="text" name='email' placeholder='enter your email' onChange={(e) => this.set_value(e)} /> <br /><br />
-                
+
                 <h3>My name is {this.state.myName} and my email is {this.state.email}</h3>
-                
+
                 {/* <button className='btn btn-primary' onClick={this.set_name}>Set Name</button><br /><br />
                 <button className='btn btn-primary' onClick={this.get_name}>Get Name</button><br /><br /> */}
 
