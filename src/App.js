33d4a91f9c8e4fc6a9be6ebca636ce792fd28b74
faizan542 <faizansilat100@gmail.com>
2022-09-16 18:17:21 +0500

    App file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
 import './App.css';
-import Header from './components/Header';
 import { ComOne, Logo } from './components/ComOne';
 import Footer from './components/Footer';
-
+import Child from './components/Child';
 
 export default class App extends Component {
+  get_data = (data) => {
+    console.log(data)
+  }
   render() {
     let todos = [{ text: "Learn react" }, { text: "Go to Gym" }]
     return (
       <div>
-        <Header />
-        
+        <ComOne />
+
         <Logo />
 
-        <h1 style={{ fontStyle: "italic", fontFamily: "fantasy", backgroundColor: "pink" }}>Main Component</h1>
+        <h1 style={{ fontStyle: "italic", fontFamily: "fantasy", backgroundColor: "pink" }}>Parent Component</h1>
+
         <ul className='todos'>
           {todos.map((item, index) => {
             return <li key={index}>{`${index + 1}  ${item.text}`}</li>
           })}
         </ul>
-        <ComOne />
+
+        <Child getData={this.get_data} name="car" price="10000" />
+        {/* <Child name="watch" price="300"/>
+            <Child name="house" price="1000000"/>
+            <Child name="bike" price="5000"/> */}
+
         <Footer />
       </div>
     )
