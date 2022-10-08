d6d37f74722328a1d94dc9299d1deab94fba910f
faizan542 <faizansilat100@gmail.com>
2022-10-08 15:37:21 +0500

    State Practice

--- src/hooks/States.jsx
+++ src/hooks/States.jsx
@@ -1,15 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 
 function States() {
-    var count = 0;
+    const [count, setCount] = useState(0);
 
-  return (
-    <div style={{textAlign: "center", backgroundColor:"lightblue", marginTop:"10px", marginBottom:"10px"}}>
-        <h1>{count}</h1>
-        <button>+</button>
-    </div>
-  )
+    function increase() {
+        setCount(count + 1)
+    }
+
+    function decrease() {
+        setCount(count - 1)
+        if(count === 0){
+            setCount(count)
+        }
+    }
+
+    return (
+        <div style={{ textAlign: "center", backgroundColor: "lightblue", marginTop: "10px", marginBottom: "10px" }}>
+            <h3>State Practice</h3>
+            <h1>{count}</h1>
+            <button onClick={decrease}>-</button>&nbsp;
+            <button onClick={increase}>+</button>
+        </div>
+    )
 }
 
 export default States
